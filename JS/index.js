af97e20d34601c4782596045f75e1cd965b1b405
Workisimportant0 <islam.ismailov2154@gmail.com>
2021-09-17 15:53:18 +0200

    fsdvcs

--- JS/index.js
+++ JS/index.js
@@ -30,6 +30,31 @@ function ajouterPostItRose() {
     }
 }
 
+function createCookie(pourBouffer,listPostIt,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = pourBouffer+"="+"listPostIt"+expires+"; path=/";
+}
+
+function readCookie(pourBouffer) {
+	var nameEQ = pourBouffer + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(pourBouffer) {
+	createCookie(pourBouffer,"",-1);
+}
+
 
 window.addEventListener("load", () => {
 
